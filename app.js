a6a762fba94bcc6bd377720e2449e9d255d66758
Prasad More <prasadmoremain@gmail.com>
2023-12-18 11:16:56 +0530

    Created Dice Game index and style file

---
+++ app.js
@@ -0,0 +1,16 @@
+let roll = () =>{
+    let random = Math.floor(Math.random());
+    let diceNo = document.getElementById('dice');
+    diceNo.innerText=(random);
+
+    setTimeout(() => {
+         rolled
+    }, 1000);
+}
+
+
+
+
+
+
+
