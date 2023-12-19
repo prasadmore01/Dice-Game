78ea18d49118ac315317b5ea3a4492c5776560fc
Prasad More <prasadmoremain@gmail.com>
2023-12-19 10:11:09 +0530

    Added Animation of dice

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 let roll = () =>{
-    let random = Math.floor(Math.random());
     let diceNo = document.getElementById('dice');
-    diceNo.innerText=(random);
+    let diceSound = document.getElementById('dice-sound')
+    diceSound.play();
+    
+    let random = Math.floor(Math.random()*6);
+    diceNo.innerText='';
+    
+    diceNo.classList.add('animate');
 
     setTimeout(() => {
-         rolled
+         diceNo.classList.remove('animate')
+         diceNo.innerText=random+1;
     }, 1000);
 }
 
